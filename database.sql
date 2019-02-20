682c41778e8f66166527498c93107665b9a2583e
alvcascac <39540415+alvcascac@users.noreply.github.com>
2019-02-20 15:31:41 +0100

    correct database

--- database.sql
+++ database.sql
@@ -8,18 +8,26 @@ create table Company(
     email CHAR(25),
     primary key(NIF)
     );
+    
+create table Route(
+    id_route integer not null,
+    category CHAR(20) not null check (category in('Prebenjamin','Benjamin','Alevín','Infantil','Cadete','Juvenil',
+    'Junior','Promesa', 'Senior','Veteranos A','Veteranos B','Veteranos C')) ,
+    distance integer,
+    price integer,
+    primary key(id_route)); 
 
 create table Running(
     id_running integer not null,
     name CHAR(15),
     dateRunning date,
     gender CHAR(10) default 'mixed' check (gender in('male','female','mixed')) ,
-    ubicacion CHAR(40),
-    NIF  CHAR(9) not null,
+    location CHAR(40),
+    id_route integer not null,
     primary key(id_running),
-    foreign key (NIF) references Company(NIF)
+    foreign key (id_route) references Route(id_route)
     );
-
+    
 create table Sponsors(
     NIF CHAR(9) not null,
     id_running integer not null,
@@ -29,18 +37,6 @@ create table Sponsors(
     foreign key(id_running) references Running(id_running)
     );
 
-create table Route(
-    id_route integer not null,
-    category CHAR(20) not null check (category in('Prebenjamin','Benjamin','Alevín','Infantil','Cadete','Juvenil',
-    'Junior','Promesa', 'Senior','Veteranos A','Veteranos B','Veteranos C')) ,
-    distance integer,
-    price integer,
-    id_running integer not null,
-    primary key(id_route),
-    foreign key (id_running) references running(id_running)
-    ); 
-
-
 create table Competitor(
     dni CHAR(9) not null,
     name CHAR(15) not null,
@@ -48,14 +44,14 @@ create table Competitor(
     email CHAR(25),
     address CHAR(25),
     nacionality CHAR(10),
-    gender CHAR(6) not null default 'mixed' check (gender in ('male', 'female', 'mixed')) ,
+    gender CHAR(6) not null check (gender in ('male', 'female')) ,
     birthday date not null,
     primary key(dni)
     );
 
 create table Participation(
     id_participation integer not null,
-    posicion integer default -1,
+    posicion integer,
     tiempo TIMESTAMP, 
     id_running integer not null,
     dni CHAR(9) not null,
