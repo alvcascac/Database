eea477d129426159b58105a7eb8ca9ed0b446791
alvcascac <39540415+alvcascac@users.noreply.github.com>
2019-02-22 11:06:56 +0100

    update

--- database.sql
+++ database.sql
@@ -1,37 +1,39 @@
-DROP DATABASE IF exists assignment1;
-CREATE DATABASE IF NOT EXISTS assignment1;
-USE assignment1;
+DROP DATABASE IF exists assignment2;
+
+CREATE DATABASE IF NOT EXISTS assignment2;
+
+USE assignment2;
+
 create table Company(
     NIF CHAR(9) not null,
-    name CHAR(15) not null,
-    phone CHAR(9),
-    email CHAR(25),
+    name CHAR(50) not null,
+    phone CHAR(50),
+    email CHAR(50),
     primary key(NIF)
     );
-    
-create table Route(
-    id_route integer not null,
-    category CHAR(20) not null check (category in('Prebenjamin','Benjamin','Alevín','Infantil','Cadete','Juvenil',
-    'Junior','Promesa', 'Senior','Veteranos A','Veteranos B','Veteranos C')) ,
-    distance integer,
-    price integer,
-    primary key(id_route)); 
 
 create table Running(
     id_running integer not null,
-    name CHAR(15),
+    name CHAR(50),
     dateRunning date,
     gender CHAR(10) default 'mixed' check (gender in('male','female','mixed')) ,
-    location CHAR(40),
-    id_route integer not null,
-    primary key(id_running),
-    foreign key (id_route) references Route(id_route)
-    );
+    location CHAR(50),
+    primary key(id_running));
     
-create table Sponsors(
+create table Route(
+    id_route integer not null,
+    category CHAR(30) not null check (category in('Prebenjamin','Benjamin','Alevín','Childish','Cadet','Youth',
+	'Senior')) ,
+    distance integer,
+    price integer,
+    id_running integer not null,
+    primary key(id_route), 
+    foreign key (id_running) references Running(id_running));
+
+create table Sponsor(
     NIF CHAR(9) not null,
     id_running integer not null,
-    sponsorDescription CHAR(200),
+    sponsorDescription CHAR(255),
     primary key(NIF, id_running),
     foreign key(NIF) references Company(NIF),
     foreign key(id_running) references Running(id_running)
@@ -39,12 +41,12 @@ create table Sponsors(
 
 create table Competitor(
     dni CHAR(9) not null,
-    name CHAR(15) not null,
-    phone CHAR(9) not null,
-    email CHAR(25),
-    address CHAR(25),
-    nacionality CHAR(10),
-    gender CHAR(6) not null check (gender in ('male', 'female')) ,
+    name CHAR(50) not null,
+    phone CHAR(50) not null,
+    email CHAR(50),
+    address CHAR(50),
+    nacionality CHAR(50),
+    gender CHAR(10) not null check (gender in ('male', 'female')) ,
     birthday date not null,
     primary key(dni)
     );
@@ -52,12 +54,13 @@ create table Competitor(
 create table Participation(
     id_participation integer not null,
     posicion integer,
-    tiempo TIMESTAMP, 
+    tiempo time, 
     id_running integer not null,
     dni CHAR(9) not null,
-    category CHAR(20) not null check (category in('Prebenjamin','Benjamin','Alevín','Infantil','Cadete','Juvenil',
-    'Junior','Promesa', 'Senior','Veteranos A','Veteranos B','Veteranos C')) ,
+    category CHAR(30) not null check (category in('Prebenjamin','Benjamin','Alevín','childish','Cadet','Youth',
+	'Senior')) ,
     primary key(id_participation),
     foreign key (id_running) references running(id_running),
     foreign key (dni) references competitor(dni)
     );
+
---
+++ insert.sql
@@ -0,0 +1,78 @@
+SET SQL_SAFE_UPDATES=0;
+USE assignment2;
+
+-- DROPS --
+
+DELETE FROM Participation;
+DELETE FROM Sponsor;
+DELETE FROM Company;
+DELETE FROM Route;
+DELETE FROM Competitor;
+DELETE FROM Running;
+
+-- Companies --
+
+INSERT INTO Company VALUES ('C11111111', 'RedBull', '+1 535 535 5335', 'redbull@xtrem.com');
+INSERT INTO Company VALUES ('C22222222', 'Cruzcampo', '+34 18 62 82 80', 'cruzcampo@customer.es');
+INSERT INTO Company VALUES ('C33333333', 'Billabong', '+1 888 258 5712', 'billabong@shop.com');
+INSERT INTO Company VALUES ('C44444444', 'Quicksilver', '+33 1 82 52 25 28', 'quicksilver@shop.com');
+
+-- Competitor --
+
+INSERT INTO Competitor VALUES ('11111111A', 'Lucas Bergeron', '+33 6 47 64 06 77', 'l.bergeron@stud.uis.no', 'Grenoble', 'French', 'male', '1997/11/26');
+INSERT INTO Competitor VALUES ('22222222A', 'Alvaro Castro', '+34 78 11 81 15', 'a.castro@stud.uis.no', 'South Spain', 'Spanish', 'male', '2001/06/28');
+INSERT INTO Competitor VALUES ('33333333A', 'Johnny Depp', '+1 000 000 0000', 'jonny.dep@fake.com', 'Kentucky', 'USA', 'male', '1995/12/03');
+INSERT INTO Competitor VALUES ('44444444A', 'Chuck Norris', '+1 123 123 8578', 'chuck.norris@fake.com', 'Texas', 'USA', 'male', '1990/04/10');
+INSERT INTO Competitor VALUES ('55555555A', 'Lukas Kurt', '+47 58 05 05 28', 'l.kurt@stud.uis.no', 'Stavanger', 'Norwegian', 'male', '2004/01/04');
+INSERT INTO Competitor VALUES ('66666666A', 'Cong Tien', '+33 01 48 96 35', 'c.tien@stud.uis.no', 'Stavanger', 'Norwegian', 'male', '2005/10/24');
+
+-- Runnings --
+
+INSERT INTO Running VALUES (1, 'St-Sylvester', '2019/12/31', 'mixed', 'Spain');
+INSERT INTO Running VALUES (2, 'NY Marathon', '2019/11/03', 'mixed', 'New York');
+INSERT INTO Running VALUES (3, 'GW of China Marathon', '2019/04/15', 'mixed', 'China');
+INSERT INTO Running VALUES (4, 'UT4M', '2020/06/20', 'mixed', 'Grenoble');
+INSERT INTO Running VALUES (5, 'WCC Slalom Men', '2020/02/11', 'male', 'Austria');
+
+-- Routes --
+
+INSERT INTO Route VALUES (1, 'Childish', 3, 10, 1);
+INSERT INTO Route VALUES (2, 'Juvenil', 42, 20, 1);
+
+INSERT INTO Route VALUES (3, 'Senior', 42, 50, 2);
+
+INSERT INTO Route VALUES (4, 'Senior', 42, 6500, 3);
+
+INSERT INTO Route VALUES (5, 'Juvenil', 21, 20, 4);
+INSERT INTO Route VALUES (6, 'Senior', 42, 50, 4);
+INSERT INTO Route VALUES (7, 'Senior', 42, 30, 4);
+
+INSERT INTO Route VALUES (8, 'Juvenil', 2, 100, 5);
+
+-- Participations --    
+
+INSERT INTO Participation VALUES (1, 3, '01:10:11', 4, '11111111A', 'senior');
+
+INSERT INTO Participation VALUES (2, 2, '00:10:11', 1, '22222222A', 'juvenil');
+
+INSERT INTO Participation VALUES (3, 15, '02:10:11', 2, '33333333A', 'senior');
+INSERT INTO Participation VALUES (4, 5, '00:50:11', 3, '33333333A', 'senior');
+
+INSERT INTO Participation VALUES (5, 1, '00:00:01', 1, '44444444A', 'senior');
+INSERT INTO Participation VALUES (6, 1, '00:00:01', 2, '44444444A', 'senior');
+INSERT INTO Participation VALUES (7, 1, '00:00:01', 3, '44444444A', 'senior');
+INSERT INTO Participation VALUES (8, 1, '00:00:01', 4, '44444444A', 'senior');
+INSERT INTO Participation VALUES (9, 1, '00:00:01', 5, '44444444A', 'senior');
+
+INSERT INTO Participation VALUES (10, 6, '01:30:20', 5, '66666666A', 'Childish');
+
+-- Sponsor --
+
+INSERT INTO Sponsor VALUES ('C11111111', 2, 'This is going to be a good race');
+INSERT INTO Sponsor VALUES ('C11111111', 3, 'We are here to support in all the materials'); 
+INSERT INTO Sponsor VALUES ('C22222222', 1, 'we love runners'); 
+INSERT INTO Sponsor VALUES ('C22222222', 4, 'I am a runner too'); 
+INSERT INTO Sponsor VALUES ('C33333333', 2, 'You can do it'); 
+INSERT INTO Sponsor VALUES ('C33333333', 4, 'Just, DO IT!'); 
+INSERT INTO Sponsor VALUES ('C44444444', 2, 'We know Chuck Norris is going to win but anyways'); 
+INSERT INTO Sponsor VALUES ('C44444444', 5, 'Join us and have fun');
